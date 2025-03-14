2d343956eed03773b844078a1ab8c0b82c1a8892
KL Tan <kailin.tan.2010@gmail.com>
2025-03-15 00:56:13 +0800

    feat: Add material ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { Typography } from "@mui/material";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 export default function Home() {
   return (
     <main>
-      <div>Next.js on GitHub Pages</div>
+      <Typography sx={{ marginLeft: 2 }} variant="h6" component="h1">
+        Apollo Music Share
+      </Typography>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
     </main>
   );
 }
