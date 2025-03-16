2d27b1a8270baa4f9d9284c3edb471fa51b3660c
KL Tan <kailin.tan.2010@gmail.com>
2025-03-16 22:03:33 +0800

    wip

--- app/components/DrinkList.tsx
+++ app/components/DrinkList.tsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAdd, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { Typography } from '@mui/material';
 
 const Item = styled(Box)(({ theme }) => ({
   ...theme.typography.body2,
@@ -26,8 +27,9 @@ type DrinkListProps = {
 };
 
 const DrinkList = ({ drinkList, onListAdd, onListRemove }: DrinkListProps) => {
-  console.log(aggregator(drinkList))
-  return <Grid container spacing={0.5} padding={1} sx={{ fontSize: 'large' }} mt={2}>
+  return <>
+    <Typography variant="h6" component="h1" sx={{ color: '#6c757d', mt: 3, pl: 2 }}>Drink List</Typography>
+    <Grid container spacing={0.5} padding={1} sx={{ fontSize: 'large' }} mt={2}>
     {Object.entries(aggregator(drinkList)).map(([drinkString, {count, drink}]: [string, { count: number, drink: Drink}]) => (
         <>
           <Grid size={8}>
@@ -43,6 +45,7 @@ const DrinkList = ({ drinkList, onListAdd, onListRemove }: DrinkListProps) => {
         </>
       ))}
   </Grid>
+  </>
 }
 
 export default DrinkList;
